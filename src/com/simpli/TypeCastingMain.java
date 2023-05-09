338cf1b87d295c45335503dc5d5aeee1f545e39b
Soumya Ranjan Rout <soumya261412@gmail.com>
2023-05-09 19:50:48 +0530

    commit

---
+++ src/com/simpli/TypeCastingMain.java
@@ -0,0 +1,28 @@
+package com.simpli;
+
+public class TypeCastingMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Implicit type casting
+		System.out.println("Implicit Type Casting :");
+		char a = 'A';
+		System.out.println("Value of a: "+a);
+		int b = a;
+		System.out.println("Value of b: "+b);
+		float c = a;
+		System.out.println("Value of c: "+c);
+		long d = a;
+		System.out.println("Value of d: "+d);
+		double e = a;
+		System.out.println("Value of e: "+e);
+		System.out.println("\n");
+		//Explicit Type casting
+		double x = 45.5;
+		int y = (int) x;
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);
+		
+	}
+
+}
